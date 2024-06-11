b2491eecc74ac1618981818dcdd925bc920e3fa0
siren <nlavrow@yandex.ru>
2024-06-11 14:56:33 +0300

    rewrite schema

--- src/main.py
+++ src/main.py
@@ -1,15 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 import motor.motor_asyncio
 
-from pydantic import BaseModel
 
 
-class User(BaseModel):
-     id: int
-     email: str
-     profile: dict[str, str]
-     city: str
+# class User(BaseModel):
+#      id: int
+#      email: str
+#      profile: dict[str, str]
+#      city: str
+
+
+class Profile:
+    def __init__(self, first_name, last_name, adress):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.adress = adress
+    
+    def to_dict(self):
+        return {
+            "email": self.first_name,
+            "profile": self.last_name,
+            "city": self.adress 
+        }
+        
+
+class User:
+    def __init__(self, email, profile, city):
+        self.email  = email
+        self.profile = profile
+        self.city = city
+    def to_dict(self):
+        return {
+            "email": self.email,
+            "profile": self.profile,
+            "city": self.city 
+        }
+
 
 app = FastAPI()
 
@@ -20,10 +47,6 @@ client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
 db = client["test_database"]
 collection = db["users"]
 
-if db != None: 
-    print('\n connection established \n')
-
-
 @app.get("/health_check")
 def health_check():
     return {"ping": "pong"}
@@ -32,21 +55,7 @@ def health_check():
 async def get_users():
      return await collection.find().to_list(None)
 
-@app.post("/create_user")
-async def create_user(user_data: User):
-    user_dict = user_data.dict(by_alias=True)
-    await collection.insert_one(user_dict) 
-    
-    return 
-    
-@app.post("/update_user")
-async def update_user(user_data: User):
-     user_dict = user_data.dict(by_alias=True)
-     await collection.update_one({"id": user_data.id}, {"$set": user_dict})
-     return 
-
-@app.post("/delete_user")
-async def delete_user(user_data: User):
-    user_dict = user_data.dict(by_alias=True)
-    await collection.delete_one({"id": user_data.id}) 
-    return 
+@app.post("/create_user", response_model=None )
+async def create_user(email: str = Body(...), profile: dict = Body(...), city: str = Body(...)):
+    result = await collection.insert_one(User(email, profile, city).to_dict())
+    return {"id": str(result.inserted_id)}
