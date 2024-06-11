9e733896c4576ceb8a14e73879e62637a59f3079
siren <nlavrow@yandex.ru>
2024-06-11 12:08:39 +0300

    fixed docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM python:3.12
-
+FROM python:3.12-book-worm
 
 WORKDIR /code
-COPY ./requirements.txt /code/requirements.txt
-RUN pip install -r requirements.txt 
-COPY ./src /code/src
 
+COPY ./requirements.txt /code/requirements.txt
+
+RUN pip install -r requirements.txt 
+
+COPY ./src /code/src
 
 
 CMD ["fastapi", "run", "src/main.py"]
--- src/main.py
+++ src/main.py
@@ -4,28 +4,19 @@ import motor.motor_asyncio
 app = FastAPI()
 
 
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://mongo_db:27017")
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
 db = client["test_database"]
 collection = db["users"]
 
-
-
-@app.get("/")
-async def root():
-    
-    return {"Hello": "word"}
+if db != None: print('\n connection established \n')
 
 
 @app.get("/health_check")
-async def health_check():
+def health_check():
     return {"ping": "pong"}
 
 @app.get("/users")
-async def users():
-    user_data = {'name' : 'John Doe','email' : 'johndoe@example.com' }
-    result = await collection.insert_one(user_data)
-    if result.inserted_id:
-        return {'message': 'success',}
-    else:
-        return {'message': 'error'}
+async def fetch_users():
+     return await collection.find().to_list(None)
+    
